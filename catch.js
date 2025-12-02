5820fdca86d138bd58340abda2cad04056498412
TampaBayWatch <mwhiting@tampabaywatch.org>
2025-12-02 15:38:57 -0500

    Update catch.js

--- catch.js
+++ catch.js
@@ -42,6 +42,7 @@ function formatReleaseDate(ds) {
 
 // Display fish card
 function showFish(f) {
+
     document.getElementById("titleHeader").style.display = "none";
 
     document.getElementById("species").textContent = f.species;
@@ -52,4 +53,65 @@ function showFish(f) {
 
     document.getElementById("originLabel").textContent = originLabel;
 
-    document.getElemen
+    document.getElementById("fishImage").src = "images/" + f.fish_image;
+
+    // Catch info
+    document.getElementById("catchInfo").innerHTML =
+        `<b>Catch Date:</b> ${getToday()}<br>
+         <b>Catch Location:</b> ${f.catch.location}<br>
+         <b>Catch Coordinates:</b> ${f.catch.coords}<br>
+         <b>Length:</b> ${f.catch.length_mm} mm (${f.catch.length_in} in)<br>
+         <b>Weight:</b> ${f.catch.weight_g} g (${f.catch.weight_lb} lbs)`;
+
+    // Hatchery extras
+    if (f.origin.toLowerCase().includes("hatch")) {
+
+        // Create the tag type name from the filename
+        let tagType = f.release.tag_image
+            .replace(".jpg", "")
+            .replace("Final", "")
+            .replace(/[_\.]/g, " ")
+            .trim()
+            .toUpperCase();
+
+        // Insert tag type text
+        document.getElementById("tagTypeText").textContent = "TAG TYPE: " + tagType;
+
+        // Release info
+        document.getElementById("releaseInfo").innerHTML =
+            `<b>Release Date:</b> ${formatReleaseDate(f.release.date)}<br>
+             <b>Release Location:</b> ${f.release.location}<br>
+             <b>Release Coordinates:</b> ${f.release.coords}<br>
+             <b>Release Length:</b> ${f.release.length_mm} mm (${f.release.length_in} in)<br>
+             <b>Release Weight:</b> ${f.release.weight_g} g`;
+
+        let tag = document.getElementById("tagImage");
+        tag.src = "images/" + f.release.tag_image;
+        tag.style.display = "block";
+
+    } else {
+        // Clear for wild fish
+        document.getElementById("releaseInfo").innerHTML = "";
+        document.getElementById("tagTypeText").textContent = "";
+        document.getElementById("tagImage").style.display = "none";
+    }
+}
+
+// Button actions
+document.getElementById("castBtn").onclick = () => {
+    fadeOut(document.getElementById("castBtn"), () => {
+        let f = pickFish();
+        showFish(f);
+        fadeIn(document.getElementById("fishCard"));
+    });
+};
+
+document.getElementById("againBtn").onclick = () => {
+    fadeOut(document.getElementById("fishCard"), () => {
+        let f = pickFish();
+        showFish(f);
+        fadeIn(document.getElementById("fishCard"));
+    });
+};
+
+loadFish();
